27ffe9f9530bd95e8fa5ceb52520930c1783f734
jaeeuncho34 <angel1041502@gmail.com>
2023-06-28 11:06:16 +0900

    Create VPC

---
+++ vpc/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 4.35"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "AWS region"
+    type        = string
+    default     = "ap-northeast-2"
+}
+
+variable "aws_profile" {
+    description = "AWS profile"
+    type = string
+    default = "jaecho"
+}
